4ffeeca216095c0e22819209d28ecdaf6ed6e321
Osiris Román <osirisr1994@gmail.com>
2020-12-11 18:20:42 -0500

    Cleaning unnecessary files and initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+
+import Header from './Components/Header/Header';
+import Formulario from './Components/Formulario/Formulario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo='Drinks and Cocktails Recipes Searcher' />
+
+      <div className='container mt-5'>
+        <div className='row'>
+          <Formulario />
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
