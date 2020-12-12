c5253139eda6bc2f79b2cc35153ea95c3432c4b2
Osiris Román <osirisr1994@gmail.com>
2020-12-11 19:20:02 -0500

    Form Initial skeleton

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
 const Formulario = () => {
-  return <h1>Formulario Aquí</h1>;
+  return (
+    <form className='col-12'>
+      <fieldset className='text-center'>
+        <legend>Search Drinks by Category or Ingredient</legend>
+      </fieldset>
+
+      <div className='row mt-4'>
+        <div className='col-md-4'>
+          <input
+            name='nombre'
+            className='form-control'
+            type='text'
+            placeholder='Search by Ingredient'
+          />
+        </div>
+        <div className='col-md-4'>
+          <select className='form-control' name='category'>
+            <option>-- Category Selection --</option>
+          </select>
+        </div>
+        <div className='col-md-4'>
+          <input
+            type='submit'
+            className='btn btn-block btn-primary'
+            value='Click to Search'
+          />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Formulario;
